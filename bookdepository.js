708db9bcf98db22280e1887bb7bd74ae4e6e8836
Josef Galea <josefeg@gmail.com>
2014-01-05 18:05:59 +0100

    popup now appears when hovering over title

--- bookdepository.js
+++ bookdepository.js
@@ -1,22 +1,67 @@
-var URL = "http://www.goodreads.com/book/review_counts.json?key=nG8gflbnH8mieOShnlnpow&format=json&isbns=";
+var QUERY_URL = "http://www.goodreads.com/book/review_counts.json?key=nG8gflbnH8mieOShnlnpow&format=json&isbns=";
+var REVIEWS_URL = "http://www.goodreads.com/book/isbn?isbn=";
 
-function handleSuccessfulResponse(titleElement, jsonResponse) {
+function displayStars(rating) {
+
+	var element = document.querySelector("div.bre_stars_container");
+	
+	var fullStarUrl = chrome.extension.getURL("full.png");
+	var halfStarUrl = chrome.extension.getURL("half.png");
+	var emptyStarUrl = chrome.extension.getURL("empty.png");
+
+	for (var i = 0; i < 5; i++) {
+
+		if (rating >= (i + 1)) {
+
+			element.innerHTML += "<img style='width: 8px; height:8px;' src='" + fullStarUrl + "'/>";
+
+		} else if (rating >= (i + 0.5)) {
+
+			element.innerHTML += "<img style='width: 8px; height:8px;' src='" + halfStarUrl + "'/>";
+
+		} else {
+
+			element.innerHTML += "<img style='width: 8px; height:8px;' src='" + emptyStarUrl + "'/>";
+		}
+	}
+}
+
+function addPopup(element, isbn, jsonResponse) {
 
 	var ratings = jsonResponse.books[0].ratings_count;
 	var averageRating = jsonResponse.books[0].average_rating;
+	var reviews = jsonResponse.books[0].reviews_count;
 
-	alert("average rating: " + averageRating + " (" + ratings + " ratings)");	
+	var goodreadsLogoUrl = chrome.extension.getURL("goodreads.png");
+
+	var tooltip = document.createElement("span");
+	tooltip.setAttribute("id", "tooltip_span");
+	tooltip.innerHTML += "<img src='" + goodreadsLogoUrl + "' style='width: 150px; height: 32px; padding-bottom: 5px;'/><br/>";
+	tooltip.innerHTML += "<div class='bre_stars_container' style='display:inline;'></div>";
+	tooltip.innerHTML += "&nbsp;" + averageRating + " (" + ratings + " ratings)<br/>";
+	tooltip.innerHTML += "<a href='" + REVIEWS_URL + isbn + "' target='_blank'>" + reviews + " reviews</a>";
+
+	var parent = element.parentNode.parentNode;
+	parent.appendChild(tooltip);
+	parent.setAttribute("class", "bre_tooltip");
+
+	displayStars(averageRating);
+}
+
+function handleSuccessfulResponse(titleElement, isbn, jsonResponse) {
+
+	addPopup(titleElement, isbn, jsonResponse);
 }
 
 function getRatings(titleElement, isbn) {
 
 	var xhr = new XMLHttpRequest();
-	xhr.open("GET", URL + isbn, true);
+	xhr.open("GET", QUERY_URL + isbn, true);
 	xhr.onreadystatechange = function() {
 
 		if ((xhr.readyState == 4) && (xhr.status == 200)) {
 		
-			handleSuccessfulResponse(titleElement, JSON.parse(xhr.responseText));
+			handleSuccessfulResponse(titleElement, isbn, JSON.parse(xhr.responseText));
 		}
 	};
 	xhr.send();
